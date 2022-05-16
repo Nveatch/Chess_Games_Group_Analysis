445811d97622178dfbf2c03aaabd2cdeddbfd542
Nveatch <95831428+Nveatch@users.noreply.github.com>
2022-05-15 20:33:59 -0500

    removed duplicate SQL files

--- drop_table.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE games;
-DROP TABLE chess_archive_data;
-DROP TABLE lichess_games_data;
-DROP TABLE chess_titles;
--- merging_tables_union_all.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE chess_data AS
-(
-SELECT * FROM games
-UNION ALL
-SELECT * FROM lichess_games_data);
