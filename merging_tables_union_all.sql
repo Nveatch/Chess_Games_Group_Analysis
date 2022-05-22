ed685409d3b9d72b8a1f91ce33c113ee11a44e84
rkappagantula <ravikiran.us@gmail.com>
2022-05-22 17:10:38 -0500

    created new user_title_table

--- merging_tables_union_all.sql
+++ merging_tables_union_all.sql
@@ -3,3 +3,10 @@ CREATE TABLE chess_data AS
 SELECT * FROM games
 UNION ALL
 SELECT * FROM lichess_games_data);
+
+CREATE TABLE user_titles AS
+(
+    select * from black_user_titles
+    UNION ALL
+    select * from white_user_titles
+);
